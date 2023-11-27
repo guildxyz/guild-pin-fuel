e0932415ca1fcf700162096cfd41743ea474a426
PopcornPaws <lipschitz77@gmail.com>
2023-11-27 14:36:49 +0100

    cool

--- pin-contract/tests/pintest/contract.rs
+++ pin-contract/tests/pintest/contract.rs
@@ -15,7 +15,41 @@ abigen!(Contract(
 pub struct GuildPinContract(GuildPin<WalletUnlocked>);
 
 impl GuildPinContract {
-    pub async fn new(parameters: Parameters) -> Self {
-        todo!()
+    pub async fn new(parameters: &Parameters) -> Self {
+        // initialize configurables
+        let configurables = GuildPinConfigurables::new()
+            .with_OWNER(Identity::Address(Address::from(parameters.owner.address())))
+            .with_TREASURY(Identity::Address(Address::from(
+                parameters.treasury.address(),
+            )))
+            .with_SIGNER(parameters.signer_b256())
+            .with_FEE(parameters.fee);
+
+        // load storage configuration
+        let storage_configuration = StorageConfiguration::default()
+            .add_slot_overrides_from_file(CONTRACT_STORAGE_PATH)
+            .unwrap();
+
+        // deploy contract
+        let configuration = LoadConfiguration::default()
+            .with_storage_configuration(storage_configuration)
+            .with_configurables(configurables);
+        let contract_id = Contract::load_from(CONTRACT_BINARY_PATH, configuration)
+            .unwrap()
+            .deploy(&parameters.owner, TxParameters::default())
+            .await
+            .unwrap();
+
+        Self(GuildPin::new(contract_id, parameters.contract.clone()))
+    }
+
+    pub async fn initialize(&self, caller: &WalletUnlocked) -> Result<FuelCallResponse<()>> {
+        self.contract
+            .with_account(caller.clone())
+            .unwrap()
+            .methods()
+            .initialize()
+            .call
+            .await
     }
 }
--- pin-contract/tests/pintest/parameters.rs
+++ pin-contract/tests/pintest/parameters.rs
@@ -1,17 +1,47 @@
-use fuels::prelude::WalletUnlocked;
-
-type EvmSigner = ();
+use fuels::prelude::{launch_custom_provider_and_get_wallets, WalletUnlocked, WalletsConfig};
+use fuels::types::Bits256;
+use signrs::eth::EthSigner;
 
 pub struct Parameters {
-    pub deployer: WalletUnlocked,
+    pub contract: WalletUnlocked,
     pub owner: WalletUnlocked,
     pub treasury: WalletUnlocked,
-    pub signer: EvmSigner,
+    pub signer: EthSigner,
     pub fee: u64,
-}
-
-pub struct Users {
     pub alice: WalletUnlocked,
     pub bob: WalletUnlocked,
     pub charlie: WalletUnlocked,
+}
+
+impl Parameters {
+    pub async fn new(fee: u64) -> Self {
+        let number_of_wallets = 6;
+        let coins_per_wallet = 1;
+        let amount_per_coin = 1_000_000_000;
+        let wallet_config = WalletsConfig::new(
+            Some(number_of_wallets),
+            Some(coins_per_wallet),
+            Some(amount_per_coin),
+        );
+        let mut wallets = launch_custom_provider_and_get_wallets(wallet_config, None, None)
+            .await
+            .unwrap();
+
+        Self {
+            contract: wallets.pop().unwrap(),
+            owner: wallets.pop().unwrap(),
+            treasury: wallets.pop().unwrap(),
+            signer: EthSigner::new(&[3u8; 32]),
+            fee,
+            alice: wallets.pop().unwrap(),
+            bob: wallets.pop().unwrap(),
+            charlie: wallets.pop().unwrap(),
+        }
+    }
+
+    pub fn signer_b256(&self) -> Bits256 {
+        let mut b256 = Bits256::zeroed();
+        b256.0[..20].copy_from_slice(&self.signer.address());
+        b256
+    }
 }
